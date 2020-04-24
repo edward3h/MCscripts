1534e7bbc33d27e1471b4d13990027089547aa00
edward3h <jaq+github@ethelred.org>
2020-04-24 08:29:38 -0400

    script to restore a server from backup. Works for me but needs a bit more work to cover all the MCscripts cases

---
+++ MCBErestorebackup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT_DIR=~mc
+
+if [ $# -lt 2 ]; then
+    echo "Usage: MCBErestorebackup.sh <server dir> <backup zip file>"
+    exit 1
+fi
+
+if [ ! -d "$ROOT_DIR/$1" ]; then
+    echo "$ROOT_DIR/$1 not found"
+    exit 2
+fi
+
+SERVICE=$1
+
+if [ ! -f "$2" ]; then
+    echo "$2 not found"
+    exit 3
+fi
+
+ZIP=$2
+
+TMP=$(mktemp -t -d 'MCBErestore.XXX')
+unzip "$ZIP" -d "$TMP"
+LEVEL_NAME=$(ls "$TMP")
+
+# workaround for backups missing 'CURRENT' file from db
+if [ ! -f "$TMP/$LEVEL_NAME/db/CURRENT" ]; then
+        (cd "$TMP/$LEVEL_NAME/db" && find . -maxdepth 1 -name 'MANIFEST*' -printf '%P\n' | tail -n1 > CURRENT)
+fi
+
+systemctl stop "mcbe@$SERVICE.service"
+WORLD_DIR="$ROOT_DIR/$SERVICE/worlds"
+DATE_PART=$(date +%Y/%b/%d_%H-%M.zip)
+TARGET_FILE="$ROOT_DIR/backup_dir/${SERVICE}_Backups/${LEVEL_NAME}_Backups/$DATE_PART"
+(cd "$WORLD_DIR"; sudo zip -r "$TARGET_FILE" "$LEVEL_NAME")
+cp -a "$TMP/$LEVEL_NAME" "$WORLD_DIR/"
+chown -R mc:nogroup "$WORLD_DIR"
+systemctl start "mcbe@$SERVICE.service"
+
+rm -r "$TMP"
